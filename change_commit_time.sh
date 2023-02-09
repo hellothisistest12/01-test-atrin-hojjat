683d1eab3352e1fd6c1aeb49f2b49d365e98e298
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-02-09 14:55:14 +0330

    New comit

--- change_commit_time.sh
+++ change_commit_time.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 git filter-branch --env-filter \
-    'if [ $GIT_COMMIT = 902344e183a03f80316a8e28d0f5d2d89e216c9a ]
+    'if [ $GIT_COMMIT = fc2b8e0ac6e9eb1c9d58f67593485bfe4f02271a ]
      then
-         export GIT_AUTHOR_DATE="Sun Feb 5 12:00:00 2023 -0700"
-         export GIT_COMMITTER_DATE="Sun Feb 5 12:00:00 2023 -0700"
+         export GIT_AUTHOR_DATE="Mon Feb 6 12:00:00 2023 -0700"
+         export GIT_COMMITTER_DATE="Mon Feb 6 12:00:00 2023 -0700"
      fi'
