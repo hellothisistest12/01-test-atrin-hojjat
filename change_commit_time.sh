d34e32b7076f359e6f26dfc9a3f43206e665f1b9
Atrin Hojjat <atrin.hojjat@gmail.com>
2023-02-09 14:55:44 +0330

    Force recreate branches

--- change_commit_time.sh
+++ change_commit_time.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git filter-branch --env-filter \
+git filter-branch -f --env-filter \
     'if [ $GIT_COMMIT = fc2b8e0ac6e9eb1c9d58f67593485bfe4f02271a ]
      then
          export GIT_AUTHOR_DATE="Mon Feb 6 12:00:00 2023 -0700"
